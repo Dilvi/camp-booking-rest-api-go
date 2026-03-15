51bd57507a67dcdc17e18cb45dce73f3b382760a
Dilvi <artem04032004@yandex.ru>
2026-03-16 01:11:55 +0300

    FileFix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/dilvi/camp-booking-rest-api-go/internal/app"
 )
 
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/health", healthHandler)
+	router := app.NewRouter()
 	fmt.Println("server started on :8080")
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
 	}
-}
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response := map[string]string {
-		"status": "ok",
-	}
-	w.WriteHeader(http.StatusOK)
-	_ =  json.NewEncoder(w).Encode(response)
 }
