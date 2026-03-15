4aa740072e28a0ab38177166ef0aad81d933a3a3
Dilvi <artem04032004@yandex.ru>
2026-03-16 00:34:01 +0300

    Create mux & create health handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/health", healthHandler)
+	fmt.Println("server started on :8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]string {
+		"status": "ok",
+	}
+	w.WriteHeader(http.StatusOK)
+	_ =  json.NewEncoder(w).Encode(response)
 }
